3cb2575764c9f34e4e923d9c6544b8d0938f9396
Kiss Cintia <kisscintiaszimonetta@ktch.hu>
2024-04-03 15:40:17 +0200

    Kezdés

---
+++ src/app.js
@@ -0,0 +1,28 @@
+/*
+* File: app.js
+* Author: Kiss Cintia
+* Copyright: 2024, Kiss Cintia
+* Group: Szoft I-2-N
+* Date: 2024-04-03
+* Github: https://github.com/KissCintia/
+* Licenc: GNU GPL
+*/
+
+const sideInput = document.querySelector('#side')
+const areaInput = document.querySelector('#area')
+const calcButton = document.querySelector('#calcButton')
+
+
+calcButton.addEventListener('click', ()=>{
+    startCalc()
+});
+
+function startCalc(){
+    const side = Number(sideInput.value)
+    const area = calcArea(side)
+    areaInput.value = area
+}
+
+function calcArea(side){
+    return 4 * Math.pow(side, 2) * Math.tan(Math.PI/16)
+}
